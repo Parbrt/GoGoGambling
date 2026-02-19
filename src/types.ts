4755939d0fcfd86533b4e35b5eb6bb1d56d170b4
Alex <a.fgx2004@gmail.com>
2026-02-19 14:43:05 +0100

    feat: add global types

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type PlayerType = {
+  id: number
+  player_name: string
+  nb_point: number
+  nb_debt: number
+  nb_share_A: number
+  avg_share_A_value: number
+  nb_share_B: number
+  avg_share_B_value: number
+  user_id: string
+  last_login: string | null
+}
